15350610c436a4511d7db3948ddbebac910a089f
Geewhiz2001 <35584292+Geewhiz2001@users.noreply.github.com>
2018-01-18 20:20:38 -0600

    Update theriteofadenos.py
    Added Highwayman scene.

--- theriteofadenos.py
+++ theriteofadenos.py
@@ -95,3 +95,129 @@ elif choice2 == "2":
 elif choice2 == "3":
     print("You pull your feet away getting off the straw bed you were laying on and walk out of the hut not saying a word.")
     
+ap = input("*You see a traveler walking toward you* \n (1) Stop the traveler and ask him why he is walking away from town \n (2) Keep walking \n")
+if ap == "1":
+    ab = input("Sir, why is it you are leaving the town.  *The man replies to your question with a confusing look* \n Because it's in the path of the demons so I thought to get as far away from here as possible. \n (1) Ask about the demons. \n (2) Be on your way.\n")
+    if ab == "1":
+        print("Wait, did you just say there are demons? \n *Man replies* Yes where have you been the past couple weeks.")
+    elif ab == "2":
+        print("Well have safe travels.")
+elif ap == "2":
+    ac = input("You continue on ignoring the traveling man. *As you're walking past him he grabs your arm* \n *The man says* Do you have a death wish or something?\n (1) Shove him off you.\n (2) Ask what hes talking about. \n")
+    if ac == "1":
+        from random import *
+        x = int(randint(1, 20))
+        print(x)
+        if x in (1, 2, 3, 4, 5) :
+            print("*You push him off you, while you do so you lose your footing and fall to the ground*")
+        elif x in (6, 7, 8, 9, 10) :
+            print("*You are barely able to shove him off you making you look as weak as a newly born child*")
+        elif x in (11, 12, 13, 14, 15) :
+            print("*You successfully push him off you*")
+        elif x in (16, 17, 18, 19) :
+            print("*You throw him off you with pushing him onto the ground*")
+        elif x in (20, 21) :
+            print("*Critical hit*")
+            from random import *
+            b = int(randint(1,20))
+            print (b)
+            if b in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) :
+                print("*You throw him off of you with such force he breaks his arm on the fall*") 
+            elif b in (11, 12, 13, 14, 15, 16, 17, 18, 19, 20) :
+                print("*You throw him off you with a god like strength, he flys back and his neck snaps like a twig when he hits the ground*")
+    elif ac == "2":
+        print("What are you talking about. \n *man replies* There are demons in that direction")
+print("*You continue on down the path toward the town*")
+watchout = input("*As you aproach the town you get stopped by a raggity looking man* \n -Highwayman- Where do you think you're going pip-squeak. \n *He shoves you back* Wheres your money. \n (1) I don't have any money. \n (2) Shove him back. \n (3) Draw your sword. \n ")
+if watchout == "1":
+    stance = input("I don't have any money. \n -Highwayman- Give me your money or I will gut you like a pig. *He pulls out a rusty knife* \n (1) I really don't have any money. \n (2) Draw out your sword. \n ")
+    if stance == "1":
+        print("*You turn out your pockets* See I don't have anything. \n -Highwayman- *The man grunts* Fine you can leave, but if I see you again I will gut you.")
+    elif stance == "2":
+        print("*You go to draw your blade*")
+        from random import *
+        x = int(randint(1, 20))
+        print(x)
+        if x in (1, 2, 3, 4, 5) :
+            print("*You fail at drawing your sword fast enough and get stabbed by the highwaymans rusty dagger*")
+        elif x in (6, 7, 8, 9, 10) :
+            print("*You are able to draw your sword and deflect the highwaymans dagger*")
+        elif x in (11, 12, 13, 14, 15) :
+            print("*You draw your sword deflecting the highwaymans attack and slicing his face in the process*")
+        elif x in (16, 17, 18, 19) :
+            print("*You draw your sword and quickly deflect his rusty dagger then thrusting your sword in his chest killing him*")
+        elif x in (20, 21) :
+            print("*Critical Hit")
+            from random import *
+            b = int(randint(1,20))
+            print (b)
+            if b in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) :
+                print("*Drawing your sword with the speed of the gods you cleave the highwaymans head clean off before he could even react*") 
+            elif b in (11, 12, 13, 14, 15, 16, 17, 18, 19, 20) :
+                print("*You draw your sword sa fast the highwayman has no time to react, it almost seems as if time has slowed at your whim* \n *You cut off his hand holding the dagger and as he screams you cut off his head with one swift movement*")
+elif watchout == "2":
+    from random import *
+    x = int(randint(1, 20))
+    print(x)
+    if x in (1, 2, 3, 4, 5) :
+        print("*You are unable to shove him off of you* \n -Highwayman- *He laughs at your attempt* You think you can get away. *He pulls out a rusty dagger*")
+        sworda = input("(1) Draw your sword \n ")
+        from random import *
+        x = int(randint(1, 20))
+        print(x)
+        if x in (1, 2, 3, 4, 5) :
+            print("*You draw your sword barley able to deflect his dagger*")
+        elif x in (6, 7, 8, 9, 10) :
+            print("*You draw your sword and deflect his attack and also slicing his cheek in the process*")
+        elif x in (11, 12, 13, 14, 15) :
+            print("*You kick him back as you draw your sword from its sheath \n after kicking him back he falls to the ground and you rush him holding your blade to his neck*")
+        elif x in (16, 17, 18, 19) :
+            print("*You draw your sword with great speed and thrust it into his cheast before he can react*")
+        elif x in (20, 21) :
+            print("Critical Hit")
+            from random import *
+            b = int(randint(1,20))
+            print (b)
+            if b in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) :
+                print("*You draw your sword deflecting the attack and in one fatial strike you behead the Highwayman*") 
+            elif b in (11, 12, 13, 14, 15, 16, 17, 18, 19, 20) :
+                print("*As you draw your sword a bolt of lightening shoots from the sky turning the man into ash*")
+    elif x in (6, 7, 8, 9, 10) :
+        print("*You shove him off of you* \n -Highwayman- *He takes a step back and pulls out a rusty dagger* ha! This will be fun.")
+        from random import *
+        x = int(randint(1, 20))
+        print(x)
+        if x in (1, 2, 3, 4, 5) :
+            print("*You disarm the man knocking his dagger to the ground* \n *The Highwayman runs away afraid what you might do to him*")
+        elif x in (6, 7, 8, 9, 10) :
+            print("*You quickly draw your sword before he can attack you, you slice at his chest wounding him* \n *He runs into the woods wounded trying not to bleed out in the process*")
+        elif x in (11, 12, 13, 14, 15) :
+            print("*You draw your sword and with one sweeping motion you cut off the Highwaymans hand* \n *The Highwayman runs off stumbling into the woods*")
+        elif x in (16, 17, 18, 19) :
+            print("*You draw your sword out of its sheath as you do this its as if time has slowed giving you more than enough time to win the fight* \n *You cut off the Highwaymans hand and do a spinning swing beheading the man with one swing*")
+        elif x in (20) :
+            print("Critical Hit")
+            from random import *
+            b = int(randint(1,20))
+            print (b)
+            if b in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) :
+                print("*As you draw your blade the man is rammed by a ox* \n *Horns plungged into the Highwayman as the ox runs by taking the man piked on his head with him*") 
+            elif b in (11, 12, 13, 14, 15, 16, 17, 18, 19, 20) :
+                print("*Before you even have the chance to draw your sword the Highwayman starts to thrust his dagger at you* \n *Before the blade even touches you a lightening bolt comes from the sky killing him instantly*")
+    elif x in (11, 12, 13, 14, 15) :
+        print("*You push him off of you throwing him back a few steps* \n -Highwayman- *He stands back* Look man I didn't mean anything by it it's just hard you can go on your way.")
+    elif x in (16, 17, 18, 19) :
+        print("*You throw him off you like hes nothing pushing him to the ground* \n -Highwayman- *He quickly stands up and runs for the woods*")
+    elif x in (20, 21) :
+        print("Critical Hit")
+        from random import *
+        b = int(randint(1,20))
+        print (b)
+        if b in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) :
+            print("*You flick him off you likes hes but only an annoying bug* -Highwayman- *He stumbles off back into the woods*") 
+        elif b in (11, 12, 13, 14, 15, 16, 17, 18, 19, 20) :
+            print("*You shove him off you throwing him several feet, not realizing your own strength you see he had cracked his head open* \n ==The Highwayman was dead==")
+elif watchout == "3":
+    print("*You pull out your sword as you do the Highwayman stumbles back afraid and runs off into the woods*")
+
+    
